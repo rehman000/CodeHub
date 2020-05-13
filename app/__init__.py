dec9340386fdfce697c86fc69892c0ece135a4da
rehman000 <rehman.arshad777@gmail.com>
2020-05-13 07:20:04 -0400

    edited -- requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -37,8 +37,8 @@ def create_app(config_class=Config):
     app.register_blueprint(main)
     app.register_blueprint(errors)
 
-    with app.app_context():               # Creates the database DB after it is deleted or removed ... 
-        db.create_all()
+    #with app.app_context():               # Creates the database DB after it is deleted or removed ... 
+    #    db.create_all()
 
     return app
 
